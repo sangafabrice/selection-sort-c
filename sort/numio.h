a209794a5581117d4e56c5eea8055393b308d95a
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-04-01 19:00:31 +0100

    ignore limit integers

--- sort/numio.h
+++ sort/numio.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 
 typedef unsigned index_t;
 typedef long number_t;
@@ -13,9 +15,10 @@ void parse_numbers(char *argv[], number_t A[], input_size_t *n)
     {
         char *argstr = *argv;
 
+        errno = 0;
         number_t a = strtol(argstr, &endptr, 10);
 
-        if (*endptr == '\0')
+        if (*endptr == '\0' && !(errno == ERANGE && (a == LONG_MAX || a == LONG_MIN)))
         {
             A[i++] = a;
         }
@@ -25,6 +28,8 @@ void parse_numbers(char *argv[], number_t A[], input_size_t *n)
             fprintf(stderr, "\x1b[93mThe argument '%s' is ignored.\x1b[0m\n", argstr);
         }
     }
+
+    errno = 0;
 }
 
 void print_numbers(number_t A[], input_size_t n)
