5270215ae09240e8a0ccae7d9cf4d4424e7c0948
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-04-02 05:21:03 +0100

    use a sentinel pointer to end loop

--- sort/selection.h
+++ sort/selection.h
@@ -1,16 +1,14 @@
 #include "numio.h"
 
-typedef unsigned unsorted_size_t;
-
-number_t *minimum_ptr(number_t A[], unsorted_size_t n_s)
+number_t *minimum_ptr(number_t A[], number_t *sentinelptr)
 {
     number_t *minptr = A;
 
-    for (number_t *ptr = &A[1]; ptr != &A[n_s]; ptr++)
+    while (++A != sentinelptr)
     {
-        if (*minptr > *ptr)
+        if (*minptr > *A)
         {
-            minptr = ptr;
+            minptr = A;
         }
     }
 
@@ -29,7 +27,7 @@ void selection_sort(number_t A[], input_size_t n)
     for (index_t s = 0; 1 < n - s; s++)
     {
         number_t *firstptr = &A[s];
-        number_t *minptr = minimum_ptr(firstptr, n - s);
+        number_t *minptr = minimum_ptr(firstptr, &A[n]);
 
         if (firstptr != minptr)
         {
